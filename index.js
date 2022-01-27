dc87a5007e954941ea84a6b1a3ed0624d00ddc55
Nate Good <nate.good@spiderstrategies.com>
2022-01-27 11:45:01 -0500

    implemented base action

--- index.js
+++ index.js
@@ -2,41 +2,51 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-const { findIssueNumber } = require('gh-action-components')
+const { findIssueNumber, configReader, BaseAction } = require('gh-action-components')
 
 const context = github.context;
-const { repository, pull_request } = context.payload
-const octokit = github.getOctokit(core.getInput('repo-token'))
-const repoOwnerParams = {
-	owner: repository.owner.login,
-	repo: repository.name
-}
+const { pull_request } = context.payload
+const { number: prNumber, base, user } = pull_request
+const baseBranch = base.ref
+const prAuthor = '@' + user.login
 
-async function run() {
-	const expectedMilestoneNumber = core.getInput('milestone-number', { required: true });
-	core.info(`expected milestoneNumber: ${expectedMilestoneNumber}`)
+class BranchCheckerAction extends BaseAction {
 
-	const issueNumber = await findIssueNumber({
-		octokit,
-		repoOwnerParams,
-		pull_request
-	})
+	async runAction() {
+		const configFile = core.getInput('config-file', { required: true });
+		const issueNumber = await findIssueNumber({action: this, pull_request})
+		const { branchNameByMilestoneNumber } = configReader(configFile)
 
-	// https://octokit.github.io/rest.js/v18#issues
-	const issueResponse = await octokit.rest.issues.get({
-		...repoOwnerParams,
-		issue_number: issueNumber
-	})
+		// https://octokit.github.io/rest.js/v18#issues
+		const issueResponse = await this.execRest(
+			(api, opts) => api.issues.get(opts),
+			{issue_number: issueNumber},
+			'Get Issue')
 
-	const { title, number:actualMilestoneNumber } = issueResponse.data.milestone
+		const { title, number: issueMilestoneNumber } = issueResponse.data.milestone
+		const issueBranch = branchNameByMilestoneNumber[issueMilestoneNumber]
 
-	if (expectedMilestoneNumber != actualMilestoneNumber) {
+		if (baseBranch != issueBranch) {
+			await this.fail(issueBranch)
+		} else {
+			core.info(`Success: PR baseBranch '${baseBranch}' matches issue #${issueNumber} ${title} branch '${issueBranch}'`)
+		}
+	}
+
+	async fail(issueBranch) {
+		const msg = `${prAuthor} it looks like this pull request is against the wrong branch.` +
+			` It should probably be ${issueBranch} instead of ${baseBranch}`
+
+		// Adds a regular comment to a pull request timeline instead of the
+		// diff view
+		await this.execRest(
+			(api, opts) => api.issues.createComment(opts),
+			{issue_number: prNumber, body: msg},
+			'Create PR comment')
+
 		// https://github.com/actions/toolkit/tree/main/packages/core#exit-codes
-		core.setFailed(`Milestone ${title} is expected for issue #${issueNumber}`);
-	} else {
-		core.info(`Milestone ${title} matches issue #$issueNumber}`)
+		core.setFailed(msg);
 	}
 }
 
-return run()
-	.catch(err => core.error(err))
+return new BranchCheckerAction().run()
